ee6cd99ca5a42d08371e296a4e47871df853f5cd
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-08-22 14:52:11 +0200

    feat: :sparkles: add line numbers
    add line number to code shown

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import 'highlight.js/lib/common';
 
+const startOfFile = 1;
+
 const fileContent = ref([]);
-const visibleLines = ref(0);
+const visibleLines = ref(startOfFile);
+
+let CodeBox;
+
+onMounted(() => {
+  CodeBox = document.getElementById("CodeBox")
+});
 
 function onFileChange(e) {
   let files = e.target.files || e.dataTransfer.files;
   if (!files.length) return;
   let LoadedFile = files[0];
   ReadFile(LoadedFile);
-  visibleLines.value = 0;
+  visibleLines.value = startOfFile;
 }
 function ReadFile(file) {
   let reader = new FileReader();
@@ -19,22 +27,15 @@ function ReadFile(file) {
     fileContent.value = e.target.result.split(/\r?\n/);
   };
   reader.readAsText(file);
-}
-
-function getCode() {
-  let code = '';
-  for (let i = 0; i < visibleLines.value; i++) {
-    if (fileContent.value[i] != undefined) {
-      code += `${AddLeadingZero(i + 1)}| ${fileContent.value[i]}\n`;
-    }
-  }
-  return code;
+  console.log(file.type);
 }
 
 function showMore() {
   if (visibleLines.value < fileContent.value.length) {
     visibleLines.value++;
   }
+
+  CodeBox.scrollTop = CodeBox.scrollHeight;
 }
 
 function showLess() {
@@ -50,7 +51,7 @@ function AddLeadingZero(num) {
 </script>
 
 <template>
-  <div class="row fixed-top border-dark border-bottom p-2 text-white text-center" id="AppNameRow">
+  <div class="row fixed-top border-dark border-bottom p-2 text-white text-center user-select-none" id="AppNameRow">
     <div class="col-md-12">
       <h1>Code Presentation Tool <i class="bi bi-code-slash"></i></h1>
     </div>
@@ -62,12 +63,17 @@ function AddLeadingZero(num) {
     <div class="col-lg-8 col-md-6 col-sm-12">
       <form @submit.prevent="onSubmit">
         <div class="mb-3">
-          <div class="border border-dark rounded p-1" id="CodeBox">
-            <highlightjs language="js" :code=getCode() />
+          <div class="border border-dark p-1" id="CodeBox">
+
+            <span v-for="line in fileContent.slice(0, visibleLines)" :key="fileContent.indexOf(line)" class="line">
+              <span class="line-number user-select-none">{{ AddLeadingZero(fileContent.indexOf(line) + 1) }}</span>
+              <highlightjs language="js" :code=line />
+            </span>
+
           </div>
         </div>
 
-        <div class="mb-3">
+        <div class=" mb-3 user-select-none">
           <label for="formFile" class="form-label">Wczytaj plik do wyświetlenia</label>
           <input class="form-control" type="file" id="formFile" @change="onFileChange">
         </div>
@@ -83,7 +89,7 @@ function AddLeadingZero(num) {
     </div>
   </div>
 
-  <div class="row fixed-bottom border-dark border-top">
+  <div class="row fixed-bottom border-dark border-top user-select-none">
     <div class="col-md-12 text-center d-flex justify-content-center align-items-center">
       <p class="pt-1">Made with <i class="bi bi-heart-fill text-danger"></i> by Mikołaj Zuziak</p>
     </div>
@@ -108,5 +114,41 @@ function AddLeadingZero(num) {
 
 #ActionButtons {
   justify-content: space-between;
+}
+
+.line {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+.line-number {
+  font-weight: lighter;
+  border-right: 1px solid #000;
+  text-align: left;
+  width: 1.5em;
+  margin-right: .5em;
+}
+
+/* ===== Scrollbar CSS ===== */
+/* Firefox */
+* {
+  scrollbar-width: auto;
+  scrollbar-color: #672dfa #5c636a;
+}
+
+/* Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 16px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #dde0e3;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #672dfa;
+  border-radius: 10px;
+  border: 2px solid #000000;
 }
 </style>
