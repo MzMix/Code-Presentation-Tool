477f3a03583d8d1221a99d5eb21966a29e60606f
Mikołaj Zuziak <miki.zuziak@gmail.com>
2022-08-30 13:33:28 +0200

    feat: :sparkles: add syntax highlighting for python with Prism
    add syntax highlight for pyton forking after building the app using Prism instead of highlight.js
    
    not highlighting syntax in build app

---
+++ src/CustomHighlight.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import { onMounted } from "vue";
+
+defineProps({
+    hCode: String,
+    hLang: String
+})
+
+// onMounted(() => {
+//     document.querySelectorAll('pre code').forEach((el) => {
+//         hljs.highlightElement(el);
+//     });
+// })
+</script>
+
+<template>
+    <pre><code>{{     props.hCode     }}</code></pre>
+</template>
